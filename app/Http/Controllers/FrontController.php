12d8b0d601d89a687daddb48f6b3e050473eed55
alonso1331 <80042663+alonso1331@users.noreply.github.com>
2023-10-10 16:14:32 +0800

    更新view連結位置

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,40 +2,48 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\News;
+use App\Models\Contact;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
 
 
 class FrontController extends Controller
 {
-    //
+
     public function index()
     {
         return view('index');
     }
 
-    public function hello()
+    public function newsList()
     {
-        // return view('hello',['id' => $id]);
-        $name = 'victoria';
-        $age = 28;
-        // return view('hello', ['name' => $name, 'age' => $age, 'id' => $id]);
-        return view('hello', compact('name', 'age'));
-    }
-
-
-    public function news()
-    {
-        $news = DB::table('news')->get();
-        return view('news', compact('news'));
+        // $news = DB::table('news')->get();
+        $news = News::get();
+        return view('front.news.list', compact('news'));
     }
 
     public function newsDetail($id)
     {
         // $news = DB::table('news')->find($id);
         // $news = DB::table('news')->where('id', $id)->get();
-        $news = DB::table('news')->where('id', $id)->first();
-        return view('detail', compact('news'));
+        // $news = DB::table('news')->where('id', $id)->first();
+        $news = News::where('id', $id)->first();
+
+        return view('front.news.detail', compact('news'));
     }
+
+    public function contact(Request $request){
+        Contact::create([
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'email' => $request->email,
+            'message' => $request->message
+        ]);
+
+        return redirect('/index');
+    }
+
 }
