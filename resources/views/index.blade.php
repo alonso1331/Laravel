97534a3032acd5c8d675dae076bc9dbcdbbceb6c
alonso1331 <80042663+alonso1331@users.noreply.github.com>
2023-10-10 16:11:33 +0800

    製作最新消息以及聯絡我們的table

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,20 @@
 @endsection
 
 @section('main')
-
+<form action="{{ asset('/contact') }}" method="post">
+    @csrf
+    <h2>聯絡我們</h2>
+    <h4>請留下您寶貴的意見</h4>
+    <label for="name">姓名</label>
+    <input type="text" name='name'>
+    <label for="phone">電話</label>
+    <input type="text" name='phone'>
+    <label for="email" >Email</label>
+    <input type="text" name='email'>
+    <label for="message">Message</label>
+    <textarea name="message" id="" cols="30" rows="10"></textarea>
+    <button type="submit">送出</button>
+</form>
 @endsection
 
 @section('js')
