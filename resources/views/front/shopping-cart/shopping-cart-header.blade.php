48441318730921833663d78cfa33db6b9ac368ba
alonso1331 <80042663+alonso1331@users.noreply.github.com>
2023-10-26 10:13:41 +0800

    更新progress bar

--- resources/views/front/shopping-cart/shopping-cart-header.blade.php
+++ resources/views/front/shopping-cart/shopping-cart-header.blade.php
@@ -1,9 +1,9 @@
 {{-- 購物車的header --}}
 <div class="cart-header">
     <h2 class="card-title fw-bolder mb-4">購物車</h2>
-    <div class="d-flex progress-block px-4 pb-4 mb-4 justify-content-around">
+    <div class="d-flex progress-block px-4 pb-4 mb-4 justify-content-center">
         {{-- 進度條 --}}
-        <div class="d-flex flex-row align-item-center col-sm-4">
+        <div class="d-flex flex-row align-item-center me-4">
             <div class="progress-title">
                 <div class="d-flex justify-content-center step step1">1</div>
                 <span class="step01">確認購物車</span>
@@ -12,7 +12,7 @@
                 <div class="progress-bar bg-success" role="progressbar" style="width: @if($step > 1)100% @elseif($step == 1)30% @else 0% @endif" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
-        <div class="d-flex flex-row align-center col-sm-4">
+        <div class="d-flex flex-row align-center me-4">
             <div class="progress-title">
                 <div class="d-flex justify-content-center step step2">2</div>
                 <span class="step02">付款與運送方式</span>
@@ -21,7 +21,7 @@
                 <div class="progress-bar bg-success" role="progressbar" style="width: @if($step > 2)100% @elseif($step == 2)30% @else 0% @endif" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
-        <div class="d-flex flex-row align-center col-sm-4">
+        <div class="d-flex flex-row align-center me-4">
             <div class="progress-title">
                 <div class="d-flex justify-content-center step step3">3</div>
                 <span class="step03">填寫資料</span>
@@ -30,7 +30,7 @@
                 <div class="progress-bar bg-success" role="progressbar" style="width: @if($step > 3)100% @elseif($step == 3)30% @else 0% @endif" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
         </div>
-        <div class="d-flex flex-column align-center progress-title col-sm-4">
+        <div class="d-flex flex-column align-center progress-title">
             <div class="d-flex justify-content-center step step4">4</div>
             <span class="step04">完成訂購</span>
         </div>
